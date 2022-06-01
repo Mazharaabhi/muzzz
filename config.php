527decc932077eaa46882007c97fa0e49845716b
Mazhar Iqbal <mazharue84@gmail.com>
2022-06-01 20:54:55 +0500

    project init

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "wwcontro_wwcontro";
+// MySQL Database Password
+$sql_db_pass = "stigawetupak";
+// MySQL Database Name
+$sql_db_name = "wwcontro_wwcontro";
+
+// Site URL
+$site_url = "https://wwcontrol.live"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "csdcsdcsd"; // Your purchase code, don't give it to anyone.
+?>
